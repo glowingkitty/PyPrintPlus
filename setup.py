42037bf6b9b2cae570b0564bd2fc73f6982db02a
marcoEDU <contact@marcobartsch.me>
2020-05-25 23:04:51 +0200

    update donate button

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyprintplus",
-    version="1.0.0",
+    version="1.0.1",
     author="Marco",
     author_email=None,
     description="A better way of printing logs in Python.",
